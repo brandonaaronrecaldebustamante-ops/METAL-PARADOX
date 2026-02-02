d2022e78ab2a6bcbcd2c70fd267666523ce625f2
Chimamire no kitsune . <brandonaaronrecaldebustamantegmail.com>
2026-02-02 11:49:08 -0500

    Convertido a PWA con soporte offline y manifest

---
+++ sw.js
@@ -0,0 +1,9 @@
+const nombreCache = 'rock-psico-v1'; 
+const archivosCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './loguito.ico' // <--- Cambiado aquí
+];
+
+/* ... el resto del código del sw.js se mantiene igual ... */
